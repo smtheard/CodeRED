3524e5632d217c0cd9bdac30daa88ecc8ec3a4ef
Stefan Theard <smtheard@gmail.com>
2015-04-04 04:11:23 -0500

    added parallax background

---
+++ MyoGame/Assets/Forest Background/BGScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+	public float scrollSpeed;
+	public float tileSizeY;
+	private Player player;
+
+	private Vector3 startPosition;
+
+	void Start () {
+		startPosition = transform.position;
+		player = GameObject.Find("Player").GetComponent<Player>();
+	}
+
+	void Update () {
+		float newPos = Mathf.Repeat (player.pos * scrollSpeed, tileSizeY);
+		transform.position = startPosition + Vector3.left *  newPos;
+	}
+}
