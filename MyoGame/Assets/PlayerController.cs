5788b546d73a14028f61357e6a78e4dcfa885c23
awfeese <feese83@gmail.com>
2015-04-04 01:26:40 -0500

    adding myo game

---
+++ MyoGame/Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+// Change the material when certain poses are made with the Myo armband.
+// Vibrate the Myo armband when a fist pose is made.
+public class PlayerController : MonoBehaviour
+{
+	public GameObject target;
+
+    // Myo game object to connect with.
+    // This object must have a ThalmicMyo script attached.
+    public GameObject myo = null;
+
+    // The pose from the last update. This is used to determine if the pose has changed
+    // so that actions are only performed upon making them rather than every frame during
+    // which they are active.
+    private Pose _lastPose = Pose.Unknown;
+
+    // Update is called once per frame.
+    void Update ()
+    {
+        // Access the ThalmicMyo component attached to the Myo game object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+
+		if (thalmicMyo.pose != _lastPose) {
+			_lastPose = thalmicMyo.pose;
+
+			if (thalmicMyo.pose == Pose.FingersSpread) {
+				target.SendMessage("OnJump", SendMessageOptions.DontRequireReceiver);
+			} 
+		} else {
+			if (thalmicMyo.pose == Pose.WaveOut) {
+				target.SendMessage("OnRight", SendMessageOptions.DontRequireReceiver);
+			} else if (thalmicMyo.pose == Pose.WaveIn) {
+				target.SendMessage("OnLeft", SendMessageOptions.DontRequireReceiver);
+			} else if (thalmicMyo.pose == Pose.Rest) {
+				target.SendMessage("OnStop", SendMessageOptions.DontRequireReceiver);
+			} 
+		}
+    }
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction (ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard) {
+            myo.Unlock (UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction ();
+    }
+}
---
+++ MyoGame/Assets/TinyWarrior/Documents/Readme.txt
@@ -0,0 +1,13 @@
+Tiny Warrior
+============
+
+Low-poly modeled.
+Rigged mecanim compatible.
+Package contains 3DMax file.
+Included side scroll demo with parallax background.
+
+Contact :
+- Homepage Link : http://buntgames.com
+- Blog Link : http://hompy.info
+- Facebook Link : http://facebook.com/buntgames
+- Email : hompy@buntgames.com
